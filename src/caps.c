a2c3bc1fe1aa98b1181938843a4e990f7adb56fb
Lewis Bourne <github@nettux.org.uk>
2026-03-26 23:41:06 +0000

    flags controlling bag and caps not working - remove

--- src/caps.c
+++ src/caps.c
@@ -22,8 +22,8 @@ u32 GetCurrentLevelCap(void)
 
     u32 i;
 
-    if (FlagGet(FLAG_NETTUX_NO_CAP))
-        return MAX_LEVEL;
+    //if (FlagGet(FLAG_NETTUX_NO_CAP))
+    //    return MAX_LEVEL;
 
     if (B_LEVEL_CAP_TYPE == LEVEL_CAP_FLAG_LIST)
     {
